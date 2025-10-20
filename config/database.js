4e38340ba634b61c668d08ec03f5b7d6b9afeca8
Marco Scheidhuber <github@mail.j23d.de>
2025-10-20 13:28:23 +0000

    Added basic database connection.

---
+++ config/database.js
@@ -0,0 +1,23 @@
+import mariadb from 'mariadb';
+import 'dotenv/config';
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.D_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+  connectTimeout: 10000
+});
+
+// Verbindung testen
+pool.getConnection()
+  .then(conn => {
+    console.log('Datenbankverbindung erfolgreich');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('Datenbankverbindung fehlgeschlagen:', err);
+  });
+
+export default pool;
