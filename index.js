2368dcd8792ffaac460b6a86191cf873ce4bac95
Marco Scheidhuber <github@mail.j23d.de>
2025-10-20 12:15:05 +0000

    Add static css file.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// Statische Dateien aus 'public'-Verzeichnis
+app.use(express.static('public'));
+
 // Startseite mit EJS Template
 app.get('/', (req, res) => {
   res.render('index', {
