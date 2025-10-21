6262cd2539af8b34408ba4f47b78520fc661a2a7
Marco Scheidhuber <github@mail.j23d.de>
2025-10-21 08:43:12 +0000

    Fix db connection.

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.D_PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   connectionLimit: 5,
   connectTimeout: 10000
